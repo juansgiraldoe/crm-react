7fdddb2fdb60a3dc9bce6b1fec2145e03a9e5601
Juan Sebastian Giraldo <juans.giraldo.e@gmail.com>
2022-06-23 23:29:38 -0500

    Implementacion de etiquetas link y mejora estetica del crm

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,22 +1,27 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, Link } from 'react-router-dom'
+import Logo from '../img/google-cloud-logo-5.png'
 
 const Layout = () => {
   return (
     <div className='md:flex md:min-h-screen'>
-      <div className='md:w-1/5 bg-blue-600 px-5 py-10'>
-        <h2 className='text-3xl font-bold text-center text-white'>CRM Clientes</h2>
+      <div className='md:w-1/6 bg-blue-600 py-10'>
+        <header className='md:flex px-2'>
+          <img src={Logo} className='w-10 md:h-auto' alt="Logo del crm" />
+          <h2 className='mx-2 text-2xl font-bold text-center text-white'>CRM Clientes</h2>
+        </header>
         <nav className='my-10'>
-          <a href="/clientes"
+          <Link to="/clientes"
           className='text-white text-2xl block mt-3 hover:bg-blue-400 py-2 bg-blue-300
-          rounded-full text-center font-semibold'
-          >Clientes</a>
-          <a href="/clientes/nuevo"
-          className='text-white text-2xl block mt-3 hover:bg-blue-400 py-2 bg-blue-300
-          rounded-full text-center font-semibold'
-          >Nuevo Cliente</a>
+          font-semibold transition-all px-2'
+          >Clientes</Link>
+          <hr/>
+          <Link to="/clientes/nuevo"
+          className='text-white text-2xl block hover:bg-blue-400 py-2 bg-blue-300
+          font-semibold transition-all px-2'
+          >Nuevo Cliente</Link>
         </nav>
       </div>
-      <div className='md:w-4/5'>
+      <div className='md:w-5/6'>
         <Outlet/>
       </div>
     </div>
